536ecef8e631d9db2c1d8cbec61f64ffd3ad7bea
Vladislav Yurikov <73247740+vlad2580@users.noreply.github.com>
2023-08-14 18:55:36 +0200

    LANG

--- src/etl/load.py
+++ src/etl/load.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 def save_to_csv(data, filepath):
-    """
-    Сохранение данных в CSV-файл.
+   """
+    Saving data to a CSV file.
 
-    Аргументы:
-    - data (DataFrame): датафрейм с данными для сохранения.
-    - filepath (str): путь к файлу, в который будут сохранены данные.
-    """
+    Arguments:
+    - data (DataFrame): dataframe with data to be saved.
+    - filepath (str): path to the file where the data will be saved.
+   """
     data.to_csv(filepath, index=False)
 
 if __name__ == "__main__":
@@ -16,12 +16,10 @@ if __name__ == "__main__":
 
     output_filepath = "data/transformed/aggregated_data.csv"
 
-    # Предположим, что result это переменная, которую вы экспортировали из transform.py
-    # Вам нужно удостовериться, что у вас есть такая переменная в transform.py и что она содержит итоговые данные.
     result = group_by_product_and_sum(remove_duplicates(extract_data("data/raw/supermarket_sales - Sheet1.csv")))
     
     if result is not None:
         save_to_csv(result, output_filepath)
-        print(f"Данные успешно сохранены в {output_filepath}")
+        print(f"Data successfully saved to {output_filepath}")
     else:
-        print("Ошибка трансформации данных. Невозможно продолжить сохранение.")
+        print("Data transformation error. Unable to continue saving")
